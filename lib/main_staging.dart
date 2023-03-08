2ba04822dca3b003e07022b57be3ced168a86f7d
Stefan <stefan@verygood.ventures>
2023-03-08 15:16:56 -0500

    first commit

---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:codemagic_test/app/app.dart';
+import 'package:codemagic_test/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
---
+++ test/app/view/app_test.dart
@@ -0,0 +1,12 @@
+import 'package:codemagic_test/app/app.dart';
+import 'package:codemagic_test/counter/counter.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('App', () {
+    testWidgets('renders CounterPage', (tester) async {
+      await tester.pumpWidget(const App());
+      expect(find.byType(CounterPage), findsOneWidget);
+    });
+  });
+}
